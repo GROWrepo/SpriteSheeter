e7dfc3bb4331cd7cd6699f7d899e57d4a612159f
댈엄지 <daramkun@live.com>
2020-03-15 11:03:49 +0900

    #1 기본 기능 구현
    - 일단은 기능 구현은 했으나 macOS에서 디버깅이 어려워서 Windows에서
      작업하기 위해 커밋
    - Windows에서 GUI 구현도 수행 예정

---
+++ SpriteSheeter.Console/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using SixLabors.ImageSharp;
+
+namespace SpriteSheeter
+{
+	class Program
+	{
+		static void PrintHelp ()
+		{
+			Console.WriteLine ("Usage:");
+			Console.WriteLine ("  ssc [--output-dir=<Output Directory>] [--output-filename=<Output Filename>] [--maximum-size=<Maximum Image Size>] <files>");
+		}
+
+		static void Main (string[] args)
+		{
+			if(args.Length == 0 || args[0] == "--help")
+			{
+				PrintHelp ();
+				return;
+			}
+
+			SpriteSheet spriteSheet = new SpriteSheet ();
+			string outputDir = "";
+			string outputFilename = "output";
+
+			foreach(var file in args)
+			{
+				if (file.StartsWith ("--output-dir="))
+				{
+					outputDir = file.Substring ("--output-dir=".Length);
+					continue;
+				}
+				else if (file.StartsWith ("--output-filename="))
+				{
+					outputFilename = file.Substring ("--output-filename=".Length);
+					continue;
+				}
+				else if (file.StartsWith ("--maximum-size="))
+				{
+					spriteSheet.MaximumSize = int.Parse (file.Substring ("--maximum-size=".Length));
+					continue;
+				}
+
+				if(!File.Exists (file))
+				{
+					Console.WriteLine ($"ERROR: File not Found: {file}");
+					continue;
+				}
+
+				try
+				{
+					spriteSheet.AddSprite (file);
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine ($"ERROR: Cannot load Image: {file} because: {ex.Message}");
+					continue;
+				}
+			}
+
+			using (Image generated = spriteSheet.GenerateSpriteSheet ())
+			{
+				using (Stream outputStream = new FileStream (Path.Combine (outputDir, $"{outputFilename}.png"), FileMode.Create))
+				{
+					generated.SaveAsPng (outputStream);
+				}
+			}
+
+			using (Stream outputStream = new FileStream (Path.Combine (outputDir, $"{outputFilename}.json"), FileMode.Create))
+			{
+				using (StreamWriter writer = new StreamWriter (outputStream, Encoding.UTF8))
+				{
+					writer.WriteLine ("{");
+					foreach (var filename in spriteSheet.Filenames)
+					{
+						var position = spriteSheet.Positions[filename];
+						var sprite = spriteSheet.Sprites[filename];
+						var size = new System.Drawing.Size (sprite.Width, sprite.Height);
+						Console.WriteLine ($"INFO: {filename}: {new System.Drawing.Rectangle (position, size)}");
+						writer.WriteLine ($"\t\"{filename}\" : \"{position.X},{position.Y},{size.Width},{size.Height}\"");
+					}
+					writer.WriteLine ("}");
+					writer.Flush ();
+				}
+			}
+		}
+	}
+}
---
+++ SpriteSheeter.Core/SpriteSheet.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.Advanced;
+using SixLabors.ImageSharp.Memory;
+using SixLabors.ImageSharp.Metadata;
+using SixLabors.ImageSharp.Formats;
+using SixLabors.ImageSharp.ColorSpaces;
+
+namespace SpriteSheeter
+{
+    public class SpriteSheet
+    {
+        int maximumSize = 4096;
+        List<string> filenames = new List<string> ();
+        Dictionary<string, Image<Rgba32>> sprites = new Dictionary<string, Image<Rgba32>> ();
+        Dictionary<string, System.Drawing.Point> positions = new Dictionary<string, System.Drawing.Point> ();
+
+        public IReadOnlyList<string> Filenames => filenames;
+        public IReadOnlyDictionary<string, Image<Rgba32>> Sprites => sprites;
+        public IReadOnlyDictionary<string, System.Drawing.Point> Positions => positions;
+
+        public int MaximumSize
+        {
+            get => maximumSize;
+            set
+            {
+                maximumSize = value;
+                Refresh ();
+            }
+        }
+
+		public SpriteSheet ()
+		{
+
+		}
+
+        public string AddSprite (string filepath)
+        {
+            string filename = System.IO.Path.GetFileName (filepath);
+
+            Image<Rgba32> image = Image.Load<Rgba32> (filepath);
+            filenames.Add (filename);
+            sprites.Add (filename, image);
+            positions.Add (filename, new System.Drawing.Point (-1, -1));
+
+            Refresh ();
+            return filename;
+        }
+
+        public void ReplaceSprite (string filename, string filepath)
+        {
+            if (filenames.Contains (filename))
+            {
+                Image<Rgba32> image = Image.Load<Rgba32> (filepath);
+                sprites[filename].Dispose ();
+                sprites[filename] = image;
+
+                Refresh ();
+            }
+        }
+
+        public void RemoveSprite (string filename)
+        {
+            if (filenames.Contains (filename))
+            {
+                filenames.Remove (filename);
+                sprites[filename].Dispose ();
+                sprites.Remove (filename);
+                positions.Remove (filename);
+
+                Refresh ();
+            }
+        }
+
+        public void Clear ()
+        {
+            filenames.Clear ();
+            foreach (var sprite in sprites)
+                sprite.Value.Dispose ();
+            sprites.Clear ();
+            positions.Clear ();
+        }
+
+        private void Refresh ()
+        {
+            int currentHorizontal = 0, currentVertical = 0;
+            int currentMaximumHeight = 0;
+
+            foreach (var sprite in sprites)
+            {
+                int nextHorizontal = currentHorizontal + sprite.Value.Width;
+
+				if(nextHorizontal > maximumSize)
+                {
+                    currentHorizontal = 0;
+                    int nextVertical = currentVertical + currentMaximumHeight;
+                    if (nextVertical > maximumSize)
+                    {
+                        positions[sprite.Key] = new System.Drawing.Point (-1, -1);
+                        break;
+                    }
+                    currentVertical = nextVertical;
+                    currentMaximumHeight = sprite.Value.Height;
+				}
+
+                positions[sprite.Key] = new System.Drawing.Point (currentHorizontal, currentVertical);
+                currentMaximumHeight = (int) Math.Max (currentMaximumHeight, sprite.Value.Height);
+
+                currentHorizontal = nextHorizontal;
+            }
+        }
+
+		public Image GenerateSpriteSheet ()
+		{
+            Rgba32[] pixels = new Rgba32[maximumSize * maximumSize];
+			foreach (var filename in filenames)
+			{
+                var sprite = sprites[filename];
+                var position = positions[filename];
+
+                for (int y = 0; y < sprite.Height; ++y)
+                {
+                    for (int x = 0; x < sprite.Width; ++x)
+                    {
+                        pixels[(y + position.Y) * maximumSize + (x + position.X)] = sprite[x, y];
+                    }
+                }
+			}
+
+            Memory<Rgba32> pixelsMemory = new Memory<Rgba32> ();
+            Image spriteSheet = Image.WrapMemory<Rgba32> (pixelsMemory, maximumSize, maximumSize);
+
+            return spriteSheet;
+		}
+    }
+}
